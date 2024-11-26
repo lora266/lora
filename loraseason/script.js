3735cac94614e6e5a344eea3c1f88ff9578fd6ed
lora266 <loraonsolana@gmail.com>
2024-11-26 04:00:24 +0300

    Add files via upload

---
+++ loraseason/script.js
@@ -0,0 +1,8 @@
+
+document.getElementById('twitter-button').addEventListener('click', () => {
+  alert('Twitter button clicked!');
+});
+
+document.getElementById('buy-button').addEventListener('click', () => {
+  alert('Buy button clicked!');
+});
